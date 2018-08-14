b98fe67c0fda2c2d9c8b79d6e382ee5f537fb589
Chris Cardle <cdcardle@gmail.com>
2018-08-14 14:52:56 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    redirect_to '/login' if !session[:name]?
+    redirect_to '/login' unless session[:name]
   end
 end
